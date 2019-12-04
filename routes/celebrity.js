66062e64d3e232c2095c48437285d897f82f3f1d
Juan Grajales <grajales.juanfe11@gmail.com>
2019-12-04 16:35:08 -0500

    complete edit profile

--- routes/celebrity.js
+++ routes/celebrity.js
@@ -36,18 +36,6 @@ router.post('/celebrities/new/save', (req, res, next)=>{
   })
 });
 
-router.get('/celebrities/edit/:celebID', (req, res, next)=>{
-  let id = req.params.celebID;
-
-  Celebrity.findById(id)
-  .then((oneCeleb)=>{
-    res.render('celebrities/edit', {celeb: oneCeleb})
-  })
-  .catch((err)=>{
-    next(err);
-  })
-})
-
 
 
 // show individual celeb details
@@ -61,12 +49,61 @@ router.get('/celebrities/:celebID', (req, res, next)=>{
   .catch((err)=>{
     next(err);
   })
+});
+
+router.get('/celebrities/edit/:celebID', (req, res, next)=>{
+  let id = req.params.celebID;
+
+  Celebrity.findById(id)
+  .then((oneCeleb)=>{
+    res.render('celebrities/edit', {celeb: oneCeleb})
+  })
+  .catch((err)=>{
+    next(err);
+  })
+});
+
+
+router.post('/celebrities/update/:id', (req, res, next)=>{
+  let id = req.params.id;
+  id = req.body.theID;
+  // i put the ID in 2 places, you can do it either way
+
+
+// i dont want you to blindly copy this route because it is fancy
+// take a hard look at whats happening or, just cblindly copy and paste the commented code
+  // Book.findByIdAndUpdate(id, {
+  //   title: req.body.title,
+  //   author: req.body.author,
+  //   image: req.body.image
+  // })
+  let newInfo = {...req.body};
+ 
+  
+  
+  // this stupid {new:true} thing is so that after we edit the book the response we get back shows us the new info isntead of the old info, not sure why this isnt the default
+  Celebrity.findByIdAndUpdate(id, newInfo, {new: true})
+  .then((response)=>{
+    console.log(response)
+    res.redirect('/celebrities/'+id)
+  })
+  .catch((err)=>{
+    next(err)
+  })
 })
 
 
 
+router.post('/celebrities/delete/:theID', (req, res, next)=>{
+  Celebrity.findByIdAndRemove(req.params.theID)
+  .then((response)=>{
+    res.redirect('/celebrities/show');
+  })
+  .catch((err)=>{
+    next(err)
+  })
 
-
+})
 
 
 
